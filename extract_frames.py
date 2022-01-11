c3c9cc0d67dacd2f3931fb520a4541882ec3962a
Raymon Shi <61591942+raymon-shi@users.noreply.github.com>
2022-01-11 11:11:47 -0500

    added live updates for both left and right arenas

--- extract_frames.py
+++ extract_frames.py
@@ -6,7 +6,6 @@ import cv2 as cv
 import os
 import re
 from ast import literal_eval
-import imutils
 import cvzone
 
 
@@ -24,7 +23,8 @@ def extract_one_frame(left_x_start, left_x_end, left_y_start, left_y_end):
 
 def extract_test_frames(left_x_start, left_x_end, left_y_start, left_y_end, interaction_dist_cm, enclosure_len_pix,
                         enclosure_len_cm, vid_fps, left_enclosure_tl, left_enclosure_tr, left_enclosure_bl,
-                        left_enclosure_br):
+                        left_enclosure_br, right_x_start, right_x_end, right_y_start, right_y_end, right_enclosure_tl,
+                        right_enclosure_tr, right_enclosure_bl, right_enclosure_br):
     file_path = filedialog.askopenfilename()
     capture = cv.VideoCapture(filename=file_path)
     save_path = filedialog.askdirectory()
@@ -37,9 +37,17 @@ def extract_test_frames(left_x_start, left_x_end, left_y_start, left_y_end, inte
     left_bl_corner = left_enclosure_bl
     left_br_corner = left_enclosure_br
 
-    sniffle_counter = 0
-    sniffle_frames = 0
-    total_frames = 0
+    right_tl_corner = right_enclosure_tl
+    right_tr_corner = right_enclosure_tr
+    right_bl_corner = right_enclosure_bl
+    right_br_corner = right_enclosure_br
+
+    left_sniffle_counter = 0
+    left_sniffle_frames = 0
+    left_total_frames = 0
+    right_sniffle_counter = 0
+    right_sniffle_frames = 0
+    right_total_frames = 0
     frame_num = 0
 
     # purple range
@@ -48,20 +56,23 @@ def extract_test_frames(left_x_start, left_x_end, left_y_start, left_y_end, inte
 
     while True:
         ret, frame = capture.read()
+        cvzone.putTextRect(frame, f'A1 Left Sniffle Counter: {left_sniffle_counter}', (25, 500), 1.5)
+        cvzone.putTextRect(frame, f'A1 Left Sniffle Time: {left_total_frames / int(vid_fps.get())}s', (25, 540), 1.5)
+        cvzone.putTextRect(frame, f'A1 Left Sniffle Frames: {left_total_frames}', (25, 580), 1.5)
+
+        cvzone.putTextRect(frame, f'A2 Right Sniffle Counter: {right_sniffle_counter}', (425, 500), 1.5)
+        cvzone.putTextRect(frame, f'A2 Right Sniffle Time: {right_total_frames / int(vid_fps.get())}s', (425, 540), 1.5)
+        cvzone.putTextRect(frame, f'A2 Right Sniffle Frames: {right_total_frames}', (425, 580), 1.5)
         if frame is None:
             break
-        cvzone.putTextRect(frame, f'Sniffle Counter: {sniffle_counter}', (25, 25), 2)
-        cvzone.putTextRect(frame, f'Sniffle Time: {total_frames / int(vid_fps.get())}s', (25, 75), 2)
-        cvzone.putTextRect(frame, f'Sniffle Frames: {total_frames}', (400, 25), 2)
 
-        # area_of_interest = frame[108:451, 56:400]
-        area_of_interest = frame[int(left_y_start.get()):int(left_y_end.get()),
-                           int(left_x_start.get()):int(left_x_end.get())]
-        hsv = cv.cvtColor(area_of_interest, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(hsv, l_range, u_range)
-        # res = cv.bitwise_and(area_of_interest, area_of_interest, mask=mask)
-        contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        for contour in contours:
+        left_area_of_interest = frame[int(left_y_start.get()):int(left_y_end.get()),
+                                int(left_x_start.get()):int(left_x_end.get())]
+        left_hsv = cv.cvtColor(left_area_of_interest, cv.COLOR_BGR2HSV)
+        left_mask = cv.inRange(left_hsv, l_range, u_range)
+        left_contours, _ = cv.findContours(left_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        for contour in left_contours:
             area = cv.contourArea(contour)
             if area > 10:
                 x, y, w, h = cv.boundingRect(contour)
@@ -69,19 +80,47 @@ def extract_test_frames(left_x_start, left_x_end, left_y_start, left_y_end, inte
                         (x + .5 * w, y + .5 * h), left_tr_corner) <= interact_dist or math.dist(
                     (x + .5 * w, y + .5 * h), left_bl_corner) <= interact_dist or math.dist(
                     (x + .5 * w, y + .5 * h), left_br_corner) <= interact_dist:
-                    cv.rectangle(area_of_interest, (x, y), (x + w, y + h), (0, 0, 255, 2))
-                    sniffle_consecutive = True
-                    sniffle_frames += 1
-                    total_frames += 1
+                    cv.rectangle(left_area_of_interest, (x, y), (x + w, y + h), (0, 0, 255, 2))
+                    left_sniffle_consecutive = True
+                    left_sniffle_frames += 1
+                    left_total_frames += 1
                 else:
-                    sniffle_consecutive = False
-                    cv.rectangle(area_of_interest, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    left_sniffle_consecutive = False
+                    cv.rectangle(left_area_of_interest, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                if not sniffle_consecutive:
-                    sniffle_frames = 0
-                if sniffle_frames == 12:
-                    sniffle_counter += 1
+                if not left_sniffle_consecutive:
+                    left_sniffle_frames = 0
+                if left_sniffle_frames == 12:
+                    left_sniffle_counter += 1
             break
+
+        right_area_of_interest = frame[int(right_y_start.get()):int(right_y_end.get()),
+                                 int(right_x_start.get()):int(right_x_end.get())]
+        right_hsv = cv.cvtColor(right_area_of_interest, cv.COLOR_BGR2HSV)
+        right_mask = cv.inRange(right_hsv, l_range, u_range)
+        right_contours, _ = cv.findContours(right_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        for contour in right_contours:
+            area = cv.contourArea(contour)
+            if area > 10:
+                x, y, w, h = cv.boundingRect(contour)
+                if math.dist((x + .5 * w, y + .5 * h), right_tl_corner) <= interact_dist or math.dist(
+                        (x + .5 * w, y + .5 * h), right_tr_corner) <= interact_dist or math.dist(
+                    (x + .5 * w, y + .5 * h), right_bl_corner) <= interact_dist or math.dist(
+                    (x + .5 * w, y + .5 * h), right_br_corner) <= interact_dist:
+                    cv.rectangle(right_area_of_interest, (x, y), (x + w, y + h), (0, 0, 255, 2))
+                    right_sniffle_consecutive = True
+                    right_sniffle_frames += 1
+                    right_total_frames += 1
+                else:
+                    right_sniffle_consecutive = False
+                    cv.rectangle(right_area_of_interest, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                if not right_sniffle_consecutive:
+                    right_sniffle_frames = 0
+                if right_sniffle_frames == 12:
+                    right_sniffle_counter += 1
+            break
+
         cv.imwrite(f"{save_path}/frame-{frame_num}.jpg", frame)
         frame_num += 1
     capture.release()
@@ -141,22 +180,22 @@ def make_extraction_buttons(tk, root):
     spacer_btn = tk.Label(root, text='')
     spacer_btn.grid(row=5, column=0)
 
-    left_x_start_all_label = tk.Label(root, text='Enter the starting x coordinate')
+    left_x_start_all_label = tk.Label(root, text='Enter the left starting x coordinate')
     left_x_start_all_entry = tk.Entry(root, width=30, justify='center')
     left_x_start_all_label.grid(row=6, column=0)
     left_x_start_all_entry.grid(row=6, column=1)
 
-    left_x_end_all_label = tk.Label(root, text='Enter the ending x coordinate')
+    left_x_end_all_label = tk.Label(root, text='Enter the left ending x coordinate')
     left_x_end_all_entry = tk.Entry(root, width=30, justify='center')
     left_x_end_all_label.grid(row=7, column=0)
     left_x_end_all_entry.grid(row=7, column=1)
 
-    left_y_start_all_label = tk.Label(root, text='Enter the starting y coordinate')
+    left_y_start_all_label = tk.Label(root, text='Enter the left starting y coordinate')
     left_y_start_all_entry = tk.Entry(root, width=30, justify='center')
     left_y_start_all_label.grid(row=8, column=0)
     left_y_start_all_entry.grid(row=8, column=1)
 
-    left_y_end_all_label = tk.Label(root, text='Enter the ending y coordinate')
+    left_y_end_all_label = tk.Label(root, text='Enter the left ending y coordinate')
     left_y_end_all_entry = tk.Entry(root, width=30, justify='center')
     left_y_end_all_label.grid(row=9, column=0)
     left_y_end_all_entry.grid(row=9, column=1)
@@ -164,51 +203,101 @@ def make_extraction_buttons(tk, root):
     spacer_btn = tk.Label(root, text='')
     spacer_btn.grid(row=10, column=0)
 
-    extraction_all_enclosure_pixel_label = tk.Label(root, text='Enter enclosure length in pixels:')
-    extraction_all_enclosure_pixel_label.grid(row=11, column=0)
-    extraction_all_enclosure_pixel_entry = tk.Entry(root, width=30, justify='center')
-    extraction_all_enclosure_pixel_entry.grid(row=11, column=1)
+    right_x_start_all_label = tk.Label(root, text='Enter the right starting x coordinate')
+    right_x_start_all_entry = tk.Entry(root, width=30, justify='center')
+    right_x_start_all_label.grid(row=11, column=0)
+    right_x_start_all_entry.grid(row=11, column=1)
 
-    extraction_all_enclosure_cm_label = tk.Label(root, text='Enter enclosure length in cm:')
-    extraction_all_enclosure_cm_label.grid(row=12, column=0)
-    extraction_all_enclosure_cm_entry = tk.Entry(root, width=30, justify='center')
-    extraction_all_enclosure_cm_entry.grid(row=12, column=1)
+    right_x_end_all_label = tk.Label(root, text='Enter the right ending x coordinate')
+    right_x_end_all_entry = tk.Entry(root, width=30, justify='center')
+    right_x_end_all_label.grid(row=12, column=0)
+    right_x_end_all_entry.grid(row=12, column=1)
 
-    extraction_all_interaction_dist_label = tk.Label(root, text='Enter interaction distance in cm:')
-    extraction_all_interaction_dist_label.grid(row=13, column=0)
-    extraction_all_interaction_dist_entry = tk.Entry(root, width=30, justify='center')
-    extraction_all_interaction_dist_entry.grid(row=13, column=1)
+    right_y_start_all_label = tk.Label(root, text='Enter the right starting y coordinate')
+    right_y_start_all_entry = tk.Entry(root, width=30, justify='center')
+    right_y_start_all_label.grid(row=13, column=0)
+    right_y_start_all_entry.grid(row=13, column=1)
 
-    extraction_all_fps_label = tk.Label(root, text='Enter the video frames per second:')
-    extraction_all_fps_label.grid(row=14, column=0)
-    extraction_all_fps_entry = tk.Entry(root, width=30, justify='center')
-    extraction_all_fps_entry.grid(row=14, column=1)
+    right_y_end_all_label = tk.Label(root, text='Enter the right ending y coordinate')
+    right_y_end_all_entry = tk.Entry(root, width=30, justify='center')
+    right_y_end_all_label.grid(row=14, column=0)
+    right_y_end_all_entry.grid(row=14, column=1)
 
     spacer_btn = tk.Label(root, text='')
     spacer_btn.grid(row=15, column=0)
 
+    extraction_all_enclosure_pixel_label = tk.Label(root, text='Enter enclosure length in pixels:')
+    extraction_all_enclosure_pixel_label.grid(row=16, column=0)
+    extraction_all_enclosure_pixel_entry = tk.Entry(root, width=30, justify='center')
+    extraction_all_enclosure_pixel_entry.grid(row=16, column=1)
+
+    extraction_all_enclosure_cm_label = tk.Label(root, text='Enter enclosure length in cm:')
+    extraction_all_enclosure_cm_label.grid(row=17, column=0)
+    extraction_all_enclosure_cm_entry = tk.Entry(root, width=30, justify='center')
+    extraction_all_enclosure_cm_entry.grid(row=17, column=1)
+
+    extraction_all_interaction_dist_label = tk.Label(root, text='Enter interaction distance in cm:')
+    extraction_all_interaction_dist_label.grid(row=18, column=0)
+    extraction_all_interaction_dist_entry = tk.Entry(root, width=30, justify='center')
+    extraction_all_interaction_dist_entry.grid(row=18, column=1)
+
+    extraction_all_fps_label = tk.Label(root, text='Enter the video frames per second:')
+    extraction_all_fps_label.grid(row=19, column=0)
+    extraction_all_fps_entry = tk.Entry(root, width=30, justify='center')
+    extraction_all_fps_entry.grid(row=19, column=1)
+
+    spacer_btn = tk.Label(root, text='')
+    spacer_btn.grid(row=20, column=0)
+
     extraction_test_enclosure_left_tl_label = tk.Label(root, text='Enter left-enclosure top-left coordinates as (x,y):')
-    extraction_test_enclosure_left_tl_label.grid(row=16, column=0)
+    extraction_test_enclosure_left_tl_label.grid(row=21, column=0)
     extraction_test_enclosure_left_tl_entry = tk.Entry(root, width=30, justify='center')
-    extraction_test_enclosure_left_tl_entry.grid(row=16, column=1)
+    extraction_test_enclosure_left_tl_entry.grid(row=21, column=1)
 
     extraction_test_enclosure_left_tr_label = tk.Label(root,
                                                        text='Enter left-enclosure top-right coordinates as (x,y):')
-    extraction_test_enclosure_left_tr_label.grid(row=17, column=0)
+    extraction_test_enclosure_left_tr_label.grid(row=22, column=0)
     extraction_test_enclosure_left_tr_entry = tk.Entry(root, width=30, justify='center')
-    extraction_test_enclosure_left_tr_entry.grid(row=17, column=1)
+    extraction_test_enclosure_left_tr_entry.grid(row=22, column=1)
 
     extraction_test_enclosure_left_bl_label = tk.Label(root,
                                                        text='Enter left-enclosure bottom-left coordinates as (x,y):')
-    extraction_test_enclosure_left_bl_label.grid(row=18, column=0)
+    extraction_test_enclosure_left_bl_label.grid(row=23, column=0)
     extraction_test_enclosure_left_bl_entry = tk.Entry(root, width=30, justify='center')
-    extraction_test_enclosure_left_bl_entry.grid(row=18, column=1)
+    extraction_test_enclosure_left_bl_entry.grid(row=23, column=1)
 
     extraction_test_enclosure_left_br_label = tk.Label(root,
                                                        text='Enter left-enclosure bottom-right coordinates as (x,y):')
-    extraction_test_enclosure_left_br_label.grid(row=19, column=0)
+    extraction_test_enclosure_left_br_label.grid(row=24, column=0)
     extraction_test_enclosure_left_br_entry = tk.Entry(root, width=30, justify='center')
-    extraction_test_enclosure_left_br_entry.grid(row=19, column=1)
+    extraction_test_enclosure_left_br_entry.grid(row=24, column=1)
+
+    spacer_btn = tk.Label(root, text='')
+    spacer_btn.grid(row=25, column=0)
+
+    extraction_test_enclosure_right_tl_label = tk.Label(root,
+                                                        text='Enter right-enclosure top-left coordinates as (x,y):')
+    extraction_test_enclosure_right_tl_label.grid(row=26, column=0)
+    extraction_test_enclosure_right_tl_entry = tk.Entry(root, width=30, justify='center')
+    extraction_test_enclosure_right_tl_entry.grid(row=26, column=1)
+
+    extraction_test_enclosure_right_tr_label = tk.Label(root,
+                                                        text='Enter right-enclosure top-right coordinates as (x,y):')
+    extraction_test_enclosure_right_tr_label.grid(row=27, column=0)
+    extraction_test_enclosure_right_tr_entry = tk.Entry(root, width=30, justify='center')
+    extraction_test_enclosure_right_tr_entry.grid(row=27, column=1)
+
+    extraction_test_enclosure_right_bl_label = tk.Label(root,
+                                                        text='Enter right-enclosure bottom-left coordinates as (x,y):')
+    extraction_test_enclosure_right_bl_label.grid(row=28, column=0)
+    extraction_test_enclosure_right_bl_entry = tk.Entry(root, width=30, justify='center')
+    extraction_test_enclosure_right_bl_entry.grid(row=28, column=1)
+
+    extraction_test_enclosure_right_br_label = tk.Label(root,
+                                                        text='Enter right-enclosure bottom-right coordinates as (x,y):')
+    extraction_test_enclosure_right_br_label.grid(row=29, column=0)
+    extraction_test_enclosure_right_br_entry = tk.Entry(root, width=30, justify='center')
+    extraction_test_enclosure_right_br_entry.grid(row=29, column=1)
 
     extraction_test_all_btn = tk.Button(root, text='Extract Test Frames',
                                         command=lambda: extract_test_frames(left_x_start_all_entry,
@@ -219,11 +308,28 @@ def make_extraction_buttons(tk, root):
                                                                             extraction_all_enclosure_pixel_entry,
                                                                             extraction_all_enclosure_cm_entry,
                                                                             extraction_all_fps_entry,
-                                                                            literal_eval(extraction_test_enclosure_left_tl_entry.get()),
-                                                                            literal_eval(extraction_test_enclosure_left_tr_entry.get()),
-                                                                            literal_eval(extraction_test_enclosure_left_bl_entry.get()),
-                                                                            literal_eval(extraction_test_enclosure_left_br_entry.get())))
-    extraction_test_all_btn.grid(row=20, column=0, columnspan=2)
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_left_tl_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_left_tr_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_left_bl_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_left_br_entry.get()),
+                                                                            right_x_start_all_entry,
+                                                                            right_x_end_all_entry,
+                                                                            right_y_start_all_entry,
+                                                                            right_y_end_all_entry,
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_right_tl_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_right_tr_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_right_bl_entry.get()),
+                                                                            literal_eval(
+                                                                                extraction_test_enclosure_right_br_entry.get())
+                                                                            ))
+    extraction_test_all_btn.grid(row=30, column=0, columnspan=2)
     extraction_convert_img_to_vid_btn = tk.Button(root, text='Convert Images to Video',
                                                   command=lambda: convert_frames_to_video(extraction_all_fps_entry))
-    extraction_convert_img_to_vid_btn.grid(row=21, column=0, columnspan=2)
+    extraction_convert_img_to_vid_btn.grid(row=31, column=0, columnspan=2)
